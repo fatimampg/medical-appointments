84241e62545a746455e567f11e11ab00aaa4d8ea
fatimampg <fatimampgouveia@gmail.com>
2025-02-27 18:08:21 +0000

    feat: add Specialization model

---
+++ db/migrate/20250227172753_create_specializations.rb
@@ -0,0 +1,9 @@
+class CreateSpecializations < ActiveRecord::Migration[8.0]
+  def change
+    create_table :specializations do |t|
+      t.string :name, null: false
+
+      t.timestamps
+    end
+  end
+end
